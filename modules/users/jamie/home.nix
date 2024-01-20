a8405d181741e0522577f2d19fbd4f85d06417a3
Jamie Allen <jamieallen050401@gmail.com>
2024-01-20 23:51:13 +0000

    Updated home.nix

--- modules/users/jamie/home.nix
+++ modules/users/jamie/home.nix
@@ -1,6 +1,23 @@
 { pkgs, osConfig, ... }:
 
-{
+let
+    homeFiles = {
+        # Name = Enable, Executable, Source, Target
+        "autostart" = [true true ../../../scripts/autostart ".scripts/autostart"];
+        "nix-rebuild" = [true true ../../../scripts/nix-rebuild ".scripts/nix-rebuild"];
+        "kde-autostart" = [true false ../../../dotfiles/autostart/autostart.desktop ".config/autostart/autostart.desktop"];
+        "git-config" = [true false ../../../dotfiles/.gitconfig ".gitconfig"];
+        "git-ignore" = [true false ../../../dotfiles/.gitignore ".gitignore"];
+        "yakuake-config" = [true false ../../../dotfiles/yakuakerc ".config/yakuakerc"];
+    };
+
+    convertHomeFile = name: values: {
+        enable = pkgs.lib.elemAt values 0;
+        executable = pkgs.lib.elemAt values 1;
+        source = pkgs.lib.elemAt values 2;
+        target = pkgs.lib.elemAt values 3;
+    };
+in {
     programs = {
         git.enable = true;
         tmux = {
@@ -80,40 +97,42 @@
             #vscodium
         ];
 
-        file = {
-            autostartScript = {
-                enable = true;
-                executable = true;
-                source = ../../../scripts/autostart;
-                target = ".scripts/autostart";
-            };
-            nixRebuildScript = {
-                enable = true;
-                executable = true;
-                source = ../../../scripts/nix-rebuild;
-                target = ".scripts/nix-rebuild";
-            };
-            autostartKDE = {
-                enable = true;
-                source = ../../../dotfiles/autostart/autostart.desktop;
-                target = ".config/autostart/autostart.desktop";
-            };
-            gitConfig = {
-                enable = true;
-                source = ../../../dotfiles/.gitconfig;
-                target = ".gitconfig";
-            };
-            gitIgnore = {
-                enable = true;
-                source = ../../../dotfiles/.gitignore;
-                target = ".gitignore";
-            };
-            yakuakeConfig = {
-                enable = true;
-                source = ../../../dotfiles/yakuakerc;
-                target = ".config/yakuakerc";
-            };
-        };
+        file = pkgs.lib.mapAttrs convertHomeFile homeFiles;
+
+#        file = {
+#            autostartScript = {
+#                enable = true;
+#                executable = true;
+#                source = ../../../scripts/autostart;
+#                target = ".scripts/autostart";
+#            };
+#            nixRebuildScript = {
+#                enable = true;
+#                executable = true;
+#                source = ../../../scripts/nix-rebuild;
+#                target = ".scripts/nix-rebuild";
+#            };
+#            autostartKDE = {
+#                enable = true;
+#                source = ../../../dotfiles/autostart/autostart.desktop;
+#                target = ".config/autostart/autostart.desktop";
+#            };
+#            gitConfig = {
+#                enable = true;
+#                source = ../../../dotfiles/.gitconfig;
+#                target = ".gitconfig";
+#            };
+#            gitIgnore = {
+#                enable = true;
+#                source = ../../../dotfiles/.gitignore;
+#                target = ".gitignore";
+#            };
+#            yakuakeConfig = {
+#                enable = true;
+#                source = ../../../dotfiles/yakuakerc;
+#                target = ".config/yakuakerc";
+#            };
+#        };
 
         stateVersion = "23.05";
     };
