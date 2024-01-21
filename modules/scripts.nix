22f8b809425f4ad20c6cf5e4cc21ed1afc82667e
Jamie Allen <jamieallen050401@gmail.com>
2024-01-21 10:47:51 +0000

    Updated scripts.nix

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -3,8 +3,9 @@
 {
     system.activationScripts = {
         ageKey.text = ''
+            storePath=$(nix-store --add-fixed sha256 "/.age/id")
             mkdir -p "${config.customOptions.userFolder}/.config/sops/age"
-            ln -f -s "/.age/id" "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
+            ln -f -s $storePath "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
         '';
     };
 }
