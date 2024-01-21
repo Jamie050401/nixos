67c5220617fedbf2cc158fb55fdc62af52935d43
Jamie Allen <jamieallen050401@gmail.com>
2024-01-21 10:44:04 +0000

    Updated scripts.nix

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -2,6 +2,9 @@
 
 {
     system.activationScripts = {
-        # ...
+        ageKey.text = ''
+            mkdir -p "${config.customOptions.userFolder}/.config/sops/age"
+            ln -f -s "/.age/id" "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
+        '';
     };
 }
