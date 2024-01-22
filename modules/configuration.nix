754773dec57fca4148cad463c83dd6af1a59a862
Jamie050401 <jamieallen050401@gmail.com>
2024-01-22 20:19:36 +0000

    Updated configuration.nix

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -129,16 +129,16 @@ in {
     };
 
     #### User Services
-#    systemd.user.services.systemUpdate = {
-#        description = "Updates/clones the local 'nixos' respository";
-#        script = ''
-#            source ${config.system.build.setEnvironment}
-#            nixRepository="/tmp/Git.Repositories/nixos"
-#            mkdir -p $nixRepository
-#            git -C $nixRepository pull || git clone "git@github.com:Jamie050401/nixos.git" $nixRepository
-#        '';
-#        wantedBy = ["graphical-session.target"];
-#    };
+    systemd.user.services.systemUpdate = {
+        description = "Updates/clones my local 'nixos' respository";
+        script = ''
+            source ${config.system.build.setEnvironment}
+            nixRepository="${config.customOptions.userFolder}/Development/Git.Repositories/nixos"
+            mkdir -p $nixRepository
+            git -C $nixRepository pull || git clone "git@github.com:Jamie050401/nixos.git" $nixRepository
+        '';
+        wantedBy = ["graphical-session.target"];
+    };
 
     #### Locale
     time.timeZone = "Europe/London";
