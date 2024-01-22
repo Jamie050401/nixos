531c916dee5ccd385e7d6933fefb5f32de313ff6
Jamie050401 <jamieallen050401@gmail.com>
2024-01-22 20:39:28 +0000

    Updated home.nix

--- modules/users/jamie/home.nix
+++ modules/users/jamie/home.nix
@@ -27,6 +27,7 @@ in {
         packages = [
             "flathub:app/com.github.tchx84.Flatseal//stable"
             "flathub:app/nz.mega.MEGAsync//stable"
+            "flathub:app/com.usebottles.bottles//stable"
         ];
         remotes = {
             "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
