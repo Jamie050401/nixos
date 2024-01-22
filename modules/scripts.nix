35cf5c8745c86cb883625561740253702b7df214
Jamie050401 <jamieallen050401@gmail.com>
2024-01-22 17:24:31 +0000

    Updated scripts.nix

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, config, ... }:
 
 {
-    #system.activationScripts = {
-    #    ageKey.text = ''
-    #        source ${config.system.build.setEnvironment}
-    #        storePath=$(nix-store --add-fixed sha256 "${config.customOptions.userFolder}/.age/id")
-    #        mkdir -p "${config.customOptions.userFolder}/.config/sops/age"
-    #        ln -f -s $storePath "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
-    #    '';
-    #};
+    system.activationScripts = {
+        ageKey.text = ''
+            source ${config.system.build.setEnvironment}
+            storePath=$(nix-store --add-fixed sha256 "${config.customOptions.userFolder}/.age/id")
+            mkdir -p "${config.customOptions.userFolder}/.config/sops/age"
+            ln -f -s $storePath "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
+        '';
+    };
 }
