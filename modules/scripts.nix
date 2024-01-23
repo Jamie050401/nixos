8459113f3c9818b44572d1bafec023a7d76e4cc4
Jamie050401 <73167537+Jamie050401@users.noreply.github.com>
2024-01-23 11:17:32 +0000

    Update scripts.nix

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -3,10 +3,10 @@
 {
     system.activationScripts = {
         ageKey.text = ''
-            PATH=$PATH:${lib.makeBinPath [ pkgs.nix pkgs.git ]}
-            storePath=$(nix-store --add-fixed sha256 "${config.customOptions.userFolder}/.age/id")
+            PATH=$PATH:${lib.makeBinPath [ pkgs.git ]}
+            agePath=${config.customOptions.userFolder}/.age/id
             mkdir -p "${config.customOptions.userFolder}/.config/sops/age"
-            ln -f -s $storePath "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
+            ln -f -s $agePath "${config.customOptions.userFolder}/.config/sops/age/keys.txt"
         '';
     };
 }
